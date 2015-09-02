ebedeecfd687367e024af23def13c325e9362051
rugbyprof <terry.griffin@mwsu.edu>
2015-09-02 16:33:06 -0500

    whatever

--- fake_load_database.php
+++ fake_load_database.php
@@ -1,1 +1,8 @@
-ha ha its empty 
+<?php
+
+$dbinfo = json_decode(file_get_contents("db.config.example.json"));
+
+print_r($dbinfo);
+$myDB = new myMysqli($dbinfo->user,$dbinfo->password,$dbinfo->host,$dbinfo->dbname);
+
+print_r($db_info);
