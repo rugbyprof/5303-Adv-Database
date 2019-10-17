6b3316d0cc1447a20c6d19b802bc57f428833ad8
Terry Griffin <terry.griffin@msutexas.edu>
2019-10-16 20:48:10 -0500

    lots of countries

---
+++ Assignments/A08/fix_countries.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python3
+"""
+This file cleans the ufo collection and removes and lat/lon pairs that are not numbers
+allowing us to create a spatial index and do distance queries.
+"""
+import pymongo  # package for working with MongoDB
+import os
+from geojson_rewind import rewind
+import json
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["armageddon"]
+countries = db["countries"]
+countries_geo = db["countries_geo"]
+
+for obj in countries.find():
+    mongo_id = obj["_id"]
+    del(obj["_id"])
+    name = obj["properties"]["ADMIN"]
+    obj["name"] = name
+
+    path = "/Users/griffin/1-Courses/5303-Adv-Database/Resources/01_Random_Data/countries/"+name+".geojson"
+
+    if not os.path.exists(path):
+        f = open(path,"w")
+        f.write(json.dumps(obj))
+        f.close()
+    else:
+        f = open(path,"r")
+        obj = f.read()
+        obj = json.loads(obj)
+        name = obj["name"]
+        del(obj["name"])
+        
+        geojson = {}
+        
+        rgeometry = rewind(obj)
+
+        geojson["loc"] = rgeometry
+        geojson["name"] = name
+        # obj["loc"]["type"] = "feature"
+
+        countries_geo.insert_one(geojson)
